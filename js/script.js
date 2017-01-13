95e92b148959ef049e813687913f84529605984b
Polina Zolotukhina <polina@iwoork.com>
2017-01-13 10:28:29 +0000

    add file

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+	$.letItSnow('.let-it-snow', {
+            stickyFlakes: 'lis-flake--js',
+            makeFlakes: true,
+        });
+
+	$(".date").hover(function(){
+		    if ($(this).find('.cover').is(':visible')) {
+	         	$(this).find('.corner').fadeIn();
+	         }
+	 }, function(){
+	    $(this).find('.corner').fadeOut();
+	});
+
+	$('.date').on('click', function(){
+		$(this).find('.cover').hide();
+		$(this).find('.corner').hide();
+	});
+});
+
+
+
